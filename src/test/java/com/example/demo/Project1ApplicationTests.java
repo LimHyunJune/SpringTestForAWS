9353cb23e94181b628bf366c33d9affb04a404be
LimHyunJune <baj2121@naver.com>
2020-01-01 13:09:45 +0900

    lastplz

--- src/test/java/com/example/demo/Project1ApplicationTests.java
+++ src/test/java/com/example/demo/Project1ApplicationTests.java
@@ -3,20 +3,29 @@ package com.example.demo;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+
+import com.example.run.Project1Application;
 
 
 
-@SpringBootTest
+@SpringBootTest(classes=Project1Application.class)
 class Project1ApplicationTests {
 	
 	
 
 	@Test
-	void contextLoads() throws Exception {
+	void contextLoads() {
 		
-	
+	System.out.print("гоюл");
 		
 	
 	}
